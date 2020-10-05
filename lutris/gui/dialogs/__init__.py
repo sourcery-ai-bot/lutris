3e70a0b5ef670128f6a24b9ab7f0217a0a2645b9
Jeremiah Wilson <jeremiahwilson777@gmail.com>
2020-10-04 22:53:59 -0700

    Add gi.require_version for GTK3

--- lutris/gui/dialogs/__init__.py
+++ lutris/gui/dialogs/__init__.py
@@ -7,6 +7,7 @@ from gettext import gettext as _
 
 import gi
 gi.require_version("WebKit2", "4.0")
+gi.require_version('Gtk', '3.0')
 from gi.repository import GObject, Gtk, WebKit2
 
 from lutris import api, settings
